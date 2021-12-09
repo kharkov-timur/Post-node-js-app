6ec4bfc9458bbed3a123e3f1ac0664718c1edd30
Timur Kharkov <kharkov.timur@gmail.com>
2021-12-09 17:42:06 +0200

    Add cors

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const methodOverride = require('method-override');
+const cors = require('cors');
+
 const postRoutes = require('./routes/post-routes');
 const postApiRoutes = require('./routes/api-post-routes');
 const pizzaApiRoutes = require('./routes/api-pizza-routes');
@@ -13,38 +15,45 @@ const createPath = require('./helpers/create-path');
 const errorMsg = chalk.bgKeyword('white').redBright;
 const successMsg = chalk.bgKeyword('green').white;
 
-const server = express();
+const app = express();
 
-server.set('view engine', 'ejs');
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
+
+app.set('view engine', 'ejs');
 
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => console.log(successMsg('Connected to DB')))
   .catch((error) => console.log(errorMsg(error)));
 
-server.listen(process.env.PORT, (error) => {
+app.listen(process.env.PORT, (error) => {
   error ? console.log(errorMsg(error)) : console.log(successMsg(`listening port ${process.env.PORT}`));
 });
 
-server.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-server.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-server.use(express.static('styles'));
+app.use(express.static('styles'));
 
-server.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
   const title = 'Home';
   res.render(createPath('index'), { title });
 });
 
-server.use(postRoutes);
-server.use(contactRoutes);
-server.use(postApiRoutes);
-server.use(pizzaApiRoutes);
+app.use(postRoutes);
+app.use(contactRoutes);
+app.use(postApiRoutes);
+app.use(pizzaApiRoutes);
 
-server.use((req, res) => {
+app.use((req, res) => {
   const title = 'Error Page';
   res
     .status(404)
