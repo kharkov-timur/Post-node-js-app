73e590d03650fc7086c79542383f4aae842fd15b
Timur Kharkov <kharkov.timur@gmail.com>
2021-12-10 10:47:21 +0200

    Add api for pizza

--- controllers/api-pizza-controller.js
+++ controllers/api-pizza-controller.js
@@ -2,16 +2,29 @@ const Pizza = require('../models/pizza');
 
 const handleError = (res, error) => {
   res.status(500).send(error.message);
-}
+};
 
 const getPizzas = (req, res) => {
-  Pizza
-    .find()
-    .sort({ createdAt: -1 })
+  const { category, sort } = req.query;
+
+  const sortBy = () => {
+    switch (sort) {
+      case 'name':
+        return { name: 1 };
+        break;
+      case 'price':
+        return { price: -1 };
+        break;
+      default:
+        return { rating: -1 };
+    }
+  };
+
+  Pizza.find(category && { category: +category })
+    .sort(sortBy())
     .then((pizzas) => res.status(200).json(pizzas))
     .catch((error) => handleError(res, error));
-}
-
+};
 
 module.exports = {
   getPizzas,
